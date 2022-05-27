6368c9c05249d63e377c6ffb94c3ab17dc6aa890
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-27 05:57:19 +0530

    Add FormModules and ReactiveFormModule for module.ts file

--- ProductManagementCRUD/src/app/app.module.ts
+++ ProductManagementCRUD/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, DialogComponent],
@@ -42,6 +43,8 @@ import { MatRadioModule } from '@angular/material/radio';
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
