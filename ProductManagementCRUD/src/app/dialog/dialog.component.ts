2e5b43650f409edf70b045b3fc944cc93537ad5f
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-27 05:55:51 +0530

    Create formControllerName values

--- ProductManagementCRUD/src/app/dialog/dialog.component.ts
+++ ProductManagementCRUD/src/app/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
   qualityList = ['Brand New', 'Second Hand', 'Refurbished'];
-  constructor() {}
+  productForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category: ['', Validators.required],
+      freshness: ['', Validators.required],
+      qty: ['', Validators.required],
+      price: ['', Validators.required],
+      date: ['', Validators.required],
+      comment: ['', Validators.required],
+    });
+  }
+
+  addProduct() {
+    console.log(this.productForm.value);
+  }
 }
+
