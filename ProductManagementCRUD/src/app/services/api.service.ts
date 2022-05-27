824913b5da1fbdb723126ce65bb5fdd078f26642
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-27 20:11:18 +0530

    Add Update Api route in service.ts fiile

--- ProductManagementCRUD/src/app/services/api.service.ts
+++ ProductManagementCRUD/src/app/services/api.service.ts
@@ -7,11 +7,17 @@ import { Injectable } from '@angular/core';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  postProduct(data : any) {
+  postProduct(data: any) {
     return this.http.post<any>('http://localhost:3000/productList/', data);
   }
 
   getProduct() {
     return this.http.get<any>('http://localhost:3000/productList/');
   }
+
+  putProduct(data: any, id: number) {
+    return this.http.put<any>('http://localhost:3000/productList/' + id, data);
+  }
+
+
 }
